c80cd35502c882760158a2b826af4f0f032a857f
chenjun <91652330+NicoIer@users.noreply.github.com>
2023-02-18 15:49:58 +0800

    (update)

---
+++ Assets/Scripts/EcsLearning/RotateCube/RotateCube.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace EcsLearning
+{
+    struct RotateSpeed: IComponentData
+    {
+        public float speed;
+    }
+    public class RotateCube : MonoBehaviour
+    {
+        public float speed = 360f;
+        public class Baker: Baker<RotateCube>
+        {
+            public override void Bake(RotateCube authoring)
+            {
+                var data = new RotateSpeed
+                {
+                    speed = math.radians(authoring.speed)
+                };
+                AddComponent(data);
+            }
+        }
+
+    }
+}
