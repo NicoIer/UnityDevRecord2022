31d0a012ff76e412f09e15abd1ccbdad5695494f
chenjun <91652330+NicoIer@users.noreply.github.com>
2023-02-18 12:50:10 +0800

    (update)迁移为ECS

---
+++ Assets/Scripts/EcsLearning/RotateCube.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace EcsLearning
+{
+    public class RotateCube : MonoBehaviour
+    {
+        [Range(0, 360)] public float rotateSpeed = 360f;
+        private void Update()
+        {
+            transform.Rotate(new Vector3(0,rotateSpeed * Time.deltaTime, 0));
+        }
+    }
+}
